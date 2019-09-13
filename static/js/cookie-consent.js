d3bf4084653333f7d9075831983e168636991725
Richie Escarez <rescarez@google.com>
2019-09-13 14:42:07 -0700

    move to partial and fix script import

--- static/js/cookie-consent.js
+++ static/js/cookie-consent.js
@@ -1,25 +1,25 @@
 // Use local copy of JavaScript Cookie v2.2.1
-$.getScript("/js/js.cookie.js", function() {
+$.getScript("/js/js.cookie.js");
+
 // Get cookie consent if the UTC time zone is within the EU+ and user has not consented previously (no cookie exists)
 $(document).ready(function() {
+  // Show consent dialog if within EU time zone and either the cookie expired or does not exist
   if ( Cookies.get('site-cookie') == null ) {
     if (testEUtimezone()) {
       toggleconsent();
-      console.log("Time zone within the EU");
-    } else {
-      console.log("Time zone is not within the EU");
     }
   } else {
     console.log("You have already consented to cookie usage");
-    testEUtimezone();
   }
 });
 // Use UTC to determine if the time zone resides in or around the EU (within UTC -1 to +6)
 function testEUtimezone(){
   var offset = new Date().getTimezoneOffset();
   if ((offset >= -360) && (offset <= 60)) { // European time zones
+    console.log("UTC time zone within the EU");
     return true;
   }
+  console.log("UTC time zone is not within the EU");
   return false; // Not EU time zone
 }
 // Consent to cookie usage. Set expiration to 1 year.
@@ -35,7 +35,7 @@ function optout() {
 }
 
 // Helpful commands (ie testing)
-            
+
 // Manually remove existing cookie
 function removecookie(){
   Cookies.remove('site-cookie');
@@ -48,4 +48,3 @@ function toggleconsent(){
 function getcookie(){
   return Cookies.get('site-cookie');
 }
-});
