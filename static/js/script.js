665a26046666111309e1df755d2f1de69d15b4e4
Markus Thömmes <markusthoemmes@me.com>
2020-07-20 17:16:18 +0200

    Add a 'Black lives matter' banner.

---
+++ static/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    html = $('html');
+
+    function flipNav() {
+        if (window.pageYOffset > 50) {
+            html.addClass("flip-nav");
+        } else {
+            html.removeClass("flip-nav");
+        }
+    }
+
+    window.addEventListener('resize', flipNav);
+    window.addEventListener('scroll', flipNav);
+});
