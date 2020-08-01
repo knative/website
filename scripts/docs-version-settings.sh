29af6cdba4d6d6c311f2de52073c45ec5a624f22
RichieEscarez <rescarez@google.com>
2020-07-31 18:13:59 -0700

    Reduce the need to set versions for each release in the scripts

--- scripts/docs-version-settings.sh
+++ scripts/docs-version-settings.sh
@@ -7,5 +7,11 @@
 # This should be set to the latest docs release/branch
 DEFAULTBRANCH="release-0.16"
 
+# Latest release version number
+LATESTVERSION="16"
+# Total number of past versions to publish
+NUMOFVERSIONS="3"
+OLDESTVERSION=$((LATESTVERSION-NUMOFVERSIONS))
+
 # An optional value that you can locally override for local builds/testing
 DEFAULTFORK="knative"
